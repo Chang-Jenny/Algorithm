1d42fe9ae4610f5e225b4b1eec44de4e399a86ee
Chang-Jenny <69885314+Chang-Jenny@users.noreply.github.com>
2022-08-10 20:40:28 +0800

    Create Fibonacci series_array.cpp

---
+++ C++/Fibonacci series_array.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    long long fib[75];
+    int n, i;
+
+    fib[1] = 1;
+    fib[2] = 1;
+    for( i=3 ; i<=70 ; i=i+1 )
+        fib[i] = fib[i-1] + fib[i-2];
+
+    while( cin >> n )
+    {
+        cout << fib[n] << endl;
+    }
+
+    return 0;
+}
