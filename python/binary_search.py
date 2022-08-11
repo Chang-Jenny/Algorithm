a24ad1b7c190cea336e24d1673d3341c2a525066
Chang-Jenny <69885314+Chang-Jenny@users.noreply.github.com>
2022-08-11 21:27:27 +0800

    Update binary_search.py

--- python/binary_search.py
+++ python/binary_search.py
@@ -1,1 +1,18 @@
-binarySearch(lst, item):
+def binary_search(lst, item):
+    low=0
+    high=len(lst)-1
+    
+    while(low <= high):
+        mid=(low + high)//2
+        choose = lst[mid]
+        print("Now choosing: lst[%d]=%d"%(mid, choose))
+        if choose == item:
+            return mid
+        elif choose > item:
+            high = mid-1
+        else:
+            low = mid+1
+    return None
+
+# lst = [1,3,5,7,9,11]
+# print(binary__search(lst, 3))
