7166ee73a34cbfda575a15e779ab0425e1a7389b
Chang-Jenny <69885314+Chang-Jenny@users.noreply.github.com>
2022-08-11 20:45:51 +0800

    Update Fibonacci_series.py

--- python/Fibonacci_series.py
+++ python/Fibonacci_series.py
@@ -1,11 +1,11 @@
 def fib(n):
-  if n==0:
-    return 0
-  elif n==1 or n==2:
-    return 1
-  else:
-    return fib(n-1)+fib(n-2)
+    if n==0:
+        return 0
+    elif n==1 or n==2:
+        return 1
+    else:
+        return fib(n-1)+fib(n-2)
 
 n = int(input("輸入要計算第幾個費氏數列: "))
 for i in range(n+1):
-  print("fib(%d)=%d"%(i,fib(i)))
+    print("fib(%d)=%d"%(i, fib(i)))
