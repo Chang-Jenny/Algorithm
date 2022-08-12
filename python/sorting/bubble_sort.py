51820f7aa6b05ed48321fcd60e6871ecedfe7b4e
Chang-Jenny <69885314+Chang-Jenny@users.noreply.github.com>
2022-08-12 19:49:33 +0800

    Add files via upload

--- python/sorting/bubble_sort.py
+++ python/sorting/bubble_sort.py
@@ -1,1 +1,32 @@
+def bubble_sort(lst):
+    # 前後相比，若前者>後者則交換位置
+    # 依序往下
+    print("initial:", end=" ")
+    for i in range(0, len(lst)):
+        print("%4d"%(lst[i]), end=" ")
+    print()
+    
+    for i in range(len(lst)-1, -1, -1):
+        for j in range(i):
+            if lst[j] > lst[j+1]:
+                lst[j], lst[j+1] = lst[j+1], lst[j]
+        print("第%d次: "%(len(lst)-i), end=" ")
+        for k in range(0, len(lst)):
+            print("%4d"%(lst[k]), end=" ")
+        print()
+        
+    print("outcome:", end=" ")
+    for i in range(0, len(lst)):
+        print("%4d"%(lst[i]), end=" ")
+        
 
+lst=list(map(int, input("Please input some number: ").split()))
+bubble_sort(lst)
+
+""" For Example: [5, 4, 3, 2, 1]
+第1次:     4    3    2    1    5
+第2次:     3    2    1    4    5
+第3次:     2    1    3    4    5
+第4次:     1    2    3    4    5
+第5次:     1    2    3    4    5
+"""
