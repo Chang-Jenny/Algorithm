a713588642cce00f1c4ad5febdea41e91898a279
Chang-Jenny <69885314+Chang-Jenny@users.noreply.github.com>
2022-10-12 20:59:37 +0800

    Update mod.py

--- python/AI/mod.py
+++ python/AI/mod.py
@@ -1,5 +1,5 @@
 import keras
-from keras.datasets import mnost
+from keras.datasets import mnist
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten
 from keras.layers import Conv2D, MaxPooling2D
